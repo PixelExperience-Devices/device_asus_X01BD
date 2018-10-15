fda031587fa2e5e01ab7b5447dabab9614161951
Rizky Benggolo <travarilo@gmail.com>
2018-10-15 07:42:27 +0000

    X01BD: rootdir: Enable Spectrum support

--- device.mk
+++ device.mk
@@ -391,7 +391,9 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.spectrum.rc \
+    init.spectrum.sh
 
 #QCOM
 PRODUCT_COPY_FILES += \

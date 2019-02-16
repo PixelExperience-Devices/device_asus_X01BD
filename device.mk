49878439ff227ea3cb3e42ba6e1f49d1cd87ebdb
Parth Bhatia <parthbhatia98@gmail.com>
2019-02-16 21:19:57 +0530

    whyred: inherit perf blobs from vendor/qcom/common

--- device.mk
+++ device.mk
@@ -406,6 +406,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
+# Performance
+include vendor/qcom/common/qti-vendor.mk
+
 # Perf Configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \

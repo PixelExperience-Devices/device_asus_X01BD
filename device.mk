001dbfe12b990b3e216006dae7ab003decd19928
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-02-10 23:11:37 +0530

    X01BD: Use TARGET_COPY_OUT_SYSTEM
    Change-Id: I4c2abb171cd6af9925680f069c3f381bd89954c4

--- device.mk
+++ device.mk
@@ -175,7 +175,7 @@ PRODUCT_PACKAGES += \
 
 # Excluded Input devices
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:system/etc/excluded-input-devices.xml
+    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/excluded-input-devices.xml
 
 # GPS / Location
 PRODUCT_PACKAGES += \
@@ -319,7 +319,7 @@ PRODUCT_COPY_FILES += \
 
 # QTI whilelistapp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # Radio
 PRODUCT_PACKAGES += \
@@ -391,7 +391,7 @@ PRODUCT_PACKAGES += \
 
 # QCOM
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:system/etc/permissions/privapp-permissions-wfd.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
 
 # QMI
 PRODUCT_PACKAGES += \

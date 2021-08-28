4c3e62365428248c45aec1ffb4bc9e5597103dea
Gaurav241 <gaurav.govinda2420@gmail.com>
2021-08-28 19:16:37 +0530

    X01BD: Don't include stock ARCORE

--- aosp_X01BD.mk
+++ aosp_X01BD.mk
@@ -16,6 +16,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ARCORE := false
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)

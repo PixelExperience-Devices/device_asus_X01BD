cab2a6041d05a1c1668fde566599d12e60e4c931
jhenrique09 <jhsv09@gmail.com>
2022-01-22 20:04:56 +0600

    X01BD: Enable quick tap support
    Change-Id: I86fe3b3ad892571f8ccc797b108dffc17019a6c9

--- aosp_X01BD.mk
+++ aosp_X01BD.mk
@@ -17,6 +17,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
